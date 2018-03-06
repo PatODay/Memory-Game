2cf7c534e50eac29f19cfb404f4178ae81620243
PatODay <poday22@gmail.com>
2018-03-06 16:38:21 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -19,15 +19,29 @@ var cards = [
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png"
 }
-];
+]
+
 var cardsInPlay = [];
 
-var flipCard = function(cardId) {
-	console.log(cards[cardId].rank);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		
+	}
+}
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage)
 	console.log(cards[cardId].suit)
-		checkForMatch();}
+		checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);}
 	var checkForMatch = function() {
 	if (cardsInPlay.length === 2)
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -35,7 +49,11 @@ var flipCard = function(cardId) {
 	} else {
 	alert("Sorry, try again.");
 }
+
+
+
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
+
